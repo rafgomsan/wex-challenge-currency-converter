aa03faf510214338028d66d35c8eaa0b2f4a5243
rafgomsan <emaildorafaelgomes@gmail.com>
2023-12-11 21:25:44 -0300

    [chore]: remove duplicated variables

--- src/main/java/com/challenge/spring/jpa/h2/dto/ConverterDto.java
+++ src/main/java/com/challenge/spring/jpa/h2/dto/ConverterDto.java
@@ -21,14 +21,10 @@ import java.math.BigDecimal;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ConverterDto implements Serializable {
 
-    static final String BASIC_EXCHANGE_RATE_URL = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/rates_of_exchange?sort=-record_date&fields=country_currency_desc,exchange_rate,record_date&filter=country_currency_desc:in:(%s,%s),record_date:lte:%s";
-    static final String TAG_DATA = "data";
-
     @Id private long id;
     private BigDecimal exchange_rate;
     private BigDecimal converted_amount;
     private String country_currency_desc;
-
     private String record_date;
     private String transaction_date;
     private String description;
